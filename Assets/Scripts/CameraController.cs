f708730061b64c5d7d6731ecd71e5f403ffbb7e0
Optimus1200 <cubanmix2u@yahoo.com>
2024-03-29 20:07:58 -0400

    added DBZ-BL sound effects sub folder

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,12 +8,6 @@ public class CameraController : MonoBehaviour
     // FIELDS *
     //*********
 
-    private GameObject cameraLeftWall;
-    private GameObject cameraRightWall;
-
-    private GameObject stageLeftWall;
-    private GameObject stageRightWall;
-
     private GameObject player1;
     private GameObject player2;
 
@@ -24,12 +18,6 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        cameraLeftWall = GameObject.Find("Main Camera");
-        cameraRightWall = GameObject.Find("Main Camera");
-
-        stageLeftWall = GameObject.Find("Stage Left Wall");
-        stageRightWall = GameObject.Find("Stage Right Wall");
-
         player1 = GameObject.Find("Player 1");
         player2 = GameObject.Find("Player 2");
     }
